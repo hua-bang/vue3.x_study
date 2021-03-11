4e803194faa2aad11d249bd48940bd63003aa24a
hua-bang <1964760211@qq.com>
2021-03-11 22:01:40 +0800

    vue-cli create vue project

---
+++ vue/cli/demo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <button @click="sub" :disabled="count<=min">-</button>
+      <input type="text" size="3" v-model="count">
+      <button @click="add" :disabled="count>=max">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "hello,vue",
+      count: 0,
+      max:10,
+      min: 1
+    }
+  },
+  methods: {
+    sub() {
+      this.count--;
+    },
+    add() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
