19a0c442592dfa8e304eb9d96918db76f9149e14
hua-bang <1964760211@qq.com>
2021-03-29 10:20:52 +0800

    composition Api 初次使用

---
+++ vue/cli/vue-composition/src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>home</div>
+  <div>{{data.count}}</div>
+  <div>{{data.double}}</div>
+  <div>
+    <button @click="addCount">add</button>
+  </div>
+</template>
+
+<script>
+import {reactive, computed} from "vue"
+
+export default {
+  name: "Home",
+  setup() {
+    const data = reactive({
+      count: 0,
+      double: computed(() => {
+        return data.count * 2;
+      })
+    });
+    const addCount = () => {
+      data.count++;
+    }
+    return {
+      data,
+      addCount
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
