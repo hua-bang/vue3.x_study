69f472ea12c4ac23a8208c64d800398357768053
hua-bang <1964760211@qq.com>
2021-04-01 09:13:09 +0800

    新建项目 配置目录别名

---
+++ vue/project/ew-shop/vue.config.js
@@ -0,0 +1,15 @@
+const path =
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "assets": '@/assets',
+                "components": '@/components',
+                "network": "@/network",
+                "utils": "@/utils",
+                "views": "@/views"
+            }
+        }
+    }
+};
